bd19108affd74cfa312d54d6d11377852c7af647
Chanoknan Sangiamjai <csangiamjai@ap.corp.jwt.com>
2019-12-25 16:20:49 +0700

    Added OAuth Instagram

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using testIg.Models;
 
 namespace testIg.Controllers
@@ -14,17 +17,74 @@ namespace testIg.Controllers
     public class AuthController : Controller
     {
         [Route("signin")]
-        public IActionResult SignIn() => View();
-
-        [Route("signin/{provider}")]
-        public IActionResult SignIn(string provider, string returnUrl = null) =>
-            Challenge(new AuthenticationProperties { RedirectUri = returnUrl ?? "/" }, provider);
-
-        [Route("signout")]
-        public async Task<IActionResult> SignOut()
+        public async Task<dynamic> auth([FromQuery] string code)
         {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Index", "Home");
+            var token = await getToken(code);
+            return JsonConvert.DeserializeObject<dynamic>(token);
         }
+
+        public async Task<dynamic> getToken(string code)
+        {
+            var client = new HttpClient();
+            string app_id = "588869918546166";
+            string app_secret = "ca64358cac14aa3c996f6dcd543589d2";
+            string grant_type = "authorization_code";
+            string redirect_uri = "https://localhost:5001/auth/signin/";
+
+            string apiUrl = "https://api.instagram.com/oauth/access_token"; //?app_id=" + app_id + "&app_secret=" + app_secret + "&grant_type=" + grant_type + "&redirect_uri=" + redirect_uri + "&code=" + code;
+
+            var values = new Dictionary<string, string> { { "app_id", app_id },
+                        { "app_secret", app_secret },
+                        { "grant_type", grant_type },
+                        { "redirect_uri", redirect_uri }
+                    };
+
+            var content = new FormUrlEncodedContent(values);
+
+
+            var response = await client.PostAsync(apiUrl, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResult = await response.Content.ReadAsStringAsync();
+
+                dynamic jsonData = JsonConvert.DeserializeObject<dynamic>(jsonResult);
+
+                return jsonData;
+            }
+            var returnDict = new Dictionary<string, dynamic>(){
+                {"success", false}
+            };
+            return returnDict;
+        }
+
+        //CURL
+        // curl -X POST \ https://api.instagram.com/oauth/access_token \ -F app_id={app-id} \ -F app_secret={app-secret} \ -F grant_type=authorization_code \ -F redirect_uri={redirect-uri} \ -F code={code}
+
+
+
+        // [Route("signin")]
+        // public IActionResult SignIn() => View();
+
+        // [Route("instagram")]
+        // public dynamic callback([FromQuery] string scope, [FromQuery] string response_type){
+        //     // string json = "";
+        //     var returnDict = new Dictionary<string, dynamic>(){
+        //         {"scope", scope},
+        //         {"response_type", response_type}
+        //     };
+        //     return JsonConvert.DeserializeObject<dynamic>(JsonConvert.SerializeObject(returnDict));
+        // }
+
+        // [Route("signin/{provider}")]
+        // public IActionResult SignIn(string provider, string returnUrl = null) =>
+        //     Challenge(new AuthenticationProperties { RedirectUri = returnUrl ?? "/" }, provider);
+
+        // [Route("signout")]
+        // public async Task<IActionResult> SignOut()
+        // {
+        //     await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        //     return RedirectToAction("Index", "Home");
+        // }
     }
 }
